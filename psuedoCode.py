02b8e0e2300ac3c17353b0fbc345c140361117c0
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-03-06 14:29:24 -0600

    Add files via upload

---
+++ psuedoCode.py
@@ -0,0 +1,18 @@
+#Factorial
+#   FUNCTION Factor
+#   DISPLAY "Enter Any Number"
+#   random_number <- Input()   
+#   FOR i in range + 1
+#       random_number <- i * random_number
+#       DISPLAY random_number
+#       END FOR
+#       CALL FUNCTION'
+
+def factor():
+    random_number = input("Enter A Number")
+    for i in range += 1:
+        random_number = i * random_number
+        print(random_number)
+
+factor()
+
